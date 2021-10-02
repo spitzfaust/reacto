8ffd6664b042fd7b790c604ea7618fe8286dbcfb
Tobias Watzek <spitzfaust@gmail.com>
2021-10-02 07:49:59 +0200

    Add SignalR front and backend
    Initial projects for the frontend and the backend
    are added that demonstrate the use of SignalR.

---
+++ backend/src/Reacto.Web/Startup.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Reacto.Web.Hubs;
+
+namespace Reacto.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:8080")
+                .WithMethods("GET", "POST")
+                .AllowAnyHeader()
+                .AllowCredentials());
+
+            app.UseRouting();
+
+            app.UseCors();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<StageHub>("/stage");
+            });
+        }
+    }
+}
