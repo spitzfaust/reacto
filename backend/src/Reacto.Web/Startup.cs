a85b7aea1a22048bc665888908d8c9ca562bba3e
Tobias Watzek <spitzfaust@gmail.com>
2021-10-05 23:00:21 +0200

    Add all azure ressources

--- backend/src/Reacto.Web/Startup.cs
+++ backend/src/Reacto.Web/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Reacto.Web.Hubs;
@@ -14,16 +15,37 @@ namespace Reacto.Web
 {
     public class Startup
     {
+        private readonly IWebHostEnvironment env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            this.env = env;
+        }
+
+        /// <summary>
+        /// Contains the configuration of the application.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
-            services.AddSignalR();
+
+            if (env.IsDevelopment())
+            {
+                services.AddSignalR();
+            }
+            else
+            {
+                services.AddSignalR().AddAzureSignalR();
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
             if (env.IsDevelopment())
             {
@@ -32,7 +54,7 @@ namespace Reacto.Web
 
             app.UseSerilogRequestLogging();
 
-            app.UseCors(builder => builder.WithOrigins("http://localhost:8080")
+            app.UseCors(builder => builder.WithOrigins(Configuration.GetValue<string>("AllowedOrigin"))
                 .WithMethods("GET", "POST")
                 .AllowAnyHeader()
                 .AllowCredentials());
@@ -40,11 +62,8 @@ namespace Reacto.Web
             app.UseRouting();
 
             app.UseCors();
-            
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapHub<StageHub>("/stage");
-            });
+
+            app.UseEndpoints(endpoints => { endpoints.MapHub<StageHub>("/stage"); });
         }
     }
 }
